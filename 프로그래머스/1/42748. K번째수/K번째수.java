611544d7bfcc04dcf5d37122121abb7b02a29fc1
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2025-10-30 23:43:38 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int[] temp = {};
+        int index = 0;
+        
+        for(int[] arr : commands){
+            // command 파싱 
+            
+            int start = arr[0] - 1;
+            int end = arr[1];
+            int position = arr[2] - 1;
+            
+            // 새로운 배열 생성 후 정렬 
+            temp = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(temp);
+
+            answer[index] = temp[position];
+            index++;
+        }
+        return answer;
+    }
+}

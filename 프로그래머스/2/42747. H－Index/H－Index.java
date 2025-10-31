825319e502b0dfe5f795c46d034cfcec5866c935
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2025-10-31 23:53:50 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        for(int i = 0; i < n; i++){
+            int hIndex = n - i;
+            
+            if(citations[i] >= hIndex){
+                return hIndex;
+            }
+        
+        }
+        return answer;
+    }
+}

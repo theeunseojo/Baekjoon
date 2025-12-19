261cd386ef9fcd7579bbf9537749c6296d80c4ac
theeunseojo <68554926+theeunseojo@users.noreply.github.com>
2025-12-19 18:50:44 +0900

    [Bronze II] Title: Hashing, Time: 184 ms, Memory: 17688 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -16,14 +16,17 @@ public class Main {
             arr[i] = i;
         }
 
-        int result = 0;
+        long result = 0;
+        long pow = 1;
         for(int i = 0; i < length; i++){
 
             int index = str.charAt(i) - 'a' + 1;
             //System.out.println(index);
-            result = (result + (int)Math.pow(31, i) * arr[index]) % 1234567891;
+
+            result +=  arr[index] * pow;
+            pow = (pow * 31) % 1234567891;
         }
 
-        System.out.println(result);
+        System.out.println(result % 1234567891);
     }
 }

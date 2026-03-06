b6eff173c49423b31ad35825694ff507bb08e4f5
eunseo cho <68554926+theeunseojo@users.noreply.github.com>
2026-03-06 23:39:21 +0900

    [Silver V] Title: 영화감독 숌, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -2,25 +2,19 @@
 
 using namespace std;
 
-int ans(int n){
-    for(int i = 666; ; i++){
-        // 666부터 시작해서 6이 3개인걸 찾는다
-
-        if(to_string(i).find("666") != -1) n--; // 찾을 경우, n 하나씩 감소
-        if(!n) return i; // n = 0일 경우, 원하는 값 리턴
-
-    }
-}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
     int n;
     cin >> n;
+    int ans;
+    for(int i = 666; ; i++){
+        if(to_string(i).find("666") != -1) n--;
+        ans = i;
+        if(n == 0) break;
+    }
 
-    cout << ans(n);
-
-
+    cout << ans;
     return 0;
 }
